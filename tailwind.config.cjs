31312edd8b98bc35941f146605b371a1b16140ab
Nazar <113494366+NazCodeland@users.noreply.github.com>
2023-03-22 14:57:14 -0400

    initial commit, project barebones setup

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			colors: {
+				squareLight: '#FFEDE4',
+				squareDark: '#732E11'
+			}
+		}
+	},
+	plugins: []
+};
